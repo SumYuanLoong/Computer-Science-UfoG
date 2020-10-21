b48d4fd7fef484bef33f8dcdf48fc78c291ed49d
Sum Yuan Loong/HomeP-PC <sumyuanloong@gmail.com>
2020-10-21 16:35:56 +0800

    some changes

---
+++ week3/digitalArithmatic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    int number,i;
+    int array[16];
+
+    printf("Hai pls enter z in (x+y)^z: ");
+    scanf("%d",&number);
+
+    while (number > 0)
+    {
+        array[i] = 
+        /* code */
+    }
+    
+}
